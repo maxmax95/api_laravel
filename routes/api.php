ff9549ee4b01fdc8dac81ba4876706a7b0f14d33
maxmax95 <onagaonaga12@gmail.com>
2023-12-26 17:33:17 -0300

    BD configurado, primeira api pronta - v1.0.0

---
+++ routes/api.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Models\Product;
+use App\Http\Controllers\Api\ProductController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/test', function(Request $request){
+    
+    ///dd($request->headers->get('Authorization'));
+
+    $response = new \Illuminate\Http\Response(json_encode(['msg' => 'first API response']));
+    $response->header('Content-type', 'application/json');
+    return $response;
+});
+
+//Products routing
+
+Route::get('/products', function(){
+    return Product::all();
+});
+
+Route::namespace('App\\Http\\Controllers\\Api\\')->group(function(){
+
+    Route::get('/products1', 'ProductController@index');
+
+});
+
+///Route::get('/products1', 'App\\Http\\Controllers\\Api\\ProductController@index');
